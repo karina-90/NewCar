6cb014051eb5c6c1d855604a8ccf759a5bd78560
Karina <ruizkarina532@gmail.com>
2021-06-13 15:52:37 -0700

    This is another commit test

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun =1;
+        FactorX.Moon=3;
+        FactorX.Planets=2;
+
+        //working on sun features
+        Sun Feron =new Sun();
+        Feron.Heat="600000000000kj";
+        Feron.Radius="823587325872385723895km/2";
+        Feron.Temperature="100000000000000000000000000f";
+    }
+}
---
+++ src/com/company/SolarSystem.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class SolarSystem {
+    public int Sun;
+    public int Moon;
+    public int Planets;
+}
